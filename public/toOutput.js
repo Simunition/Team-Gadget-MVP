ac1eef2b92bc1fb9243e46ca03601ea9231001b0
Elizabeth Sims <sims895@gmail.com>
2021-10-26 15:23:12 -0500

    Update 5

--- public/toOutput.js
+++ public/toOutput.js
@@ -14,3 +14,29 @@ function toOutput(who, what, when1, when2, where, why, impact) {
         " " + why + " " + impact + " ";
     document.getElementById('output').value = output;
 }
+
+function submitData() {
+    var who = document.getElementById("who").value;
+    var what = document.getElementById("what").value;
+    var when1 = document.getElementById("when").value;
+    var when2 = document.getElementById("when2").value;
+    var where = document.getElementById("where").value;
+    var why = document.getElementById("why").value;
+    var impact = document.getElementById("impact").value;
+    var additional = document.getElementById("additional").value;
+    var output = document.getElementById("output").value;
+
+    data = {who, what, when1, when2, where, why, impact, additional, output}
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    const response = await fetch('/api', options);
+    const json = await response.json();
+    console.log(json);
+}

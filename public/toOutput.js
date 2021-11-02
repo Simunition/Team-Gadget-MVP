d0de7b95c078affcd514b841ddf47903cc826a4c
Elizabeth Sims <sims895@gmail.com>
2021-11-02 16:29:20 -0500

    Update 7

--- public/toOutput.js
+++ public/toOutput.js
@@ -17,28 +17,28 @@ function toOutput(intro, who, what, when1, when2, where, why, impact, additional
     document.getElementById('output').value = output;
 }
 
-function submitData() {
-    var who = document.getElementById("who").value;
-    var what = document.getElementById("what").value;
-    var when1 = document.getElementById("when").value;
-    var when2 = document.getElementById("when2").value;
-    var where = document.getElementById("where").value;
-    var why = document.getElementById("why").value;
-    var impact = document.getElementById("impact").value;
-    var additional = document.getElementById("additional").value;
-    var output = document.getElementById("output").value;
+// function submitData() {
+//     var who = document.getElementById("who").value;
+//     var what = document.getElementById("what").value;
+//     var when1 = document.getElementById("when").value;
+//     var when2 = document.getElementById("when2").value;
+//     var where = document.getElementById("where").value;
+//     var why = document.getElementById("why").value;
+//     var impact = document.getElementById("impact").value;
+//     var additional = document.getElementById("additional").value;
+//     var output = document.getElementById("output").value;
 
-    data = {who, what, when1, when2, where, why, impact, additional, output}
+//     data = {who, what, when1, when2, where, why, impact, additional, output}
 
-    const options = {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    };
+//     const options = {
+//         method: "POST",
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     };
 
-    const response = await fetch('/api', options);
-    const json = await response.json();
-    console.log(json);
-}
+//     const response = await fetch('/api', options);
+//     const json = await response.json();
+//     console.log(json);
+// }

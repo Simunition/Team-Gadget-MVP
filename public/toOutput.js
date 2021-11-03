564741e21713906472e12c6e16b74ba64ce79638
Elizabeth Sims <sims895@gmail.com>
2021-11-03 16:31:31 -0500

    Update 8

--- public/toOutput.js
+++ public/toOutput.js
@@ -2,7 +2,7 @@ function getInput(){
     var intro = document.getElementById("intro").value;
     var who = document.getElementById("who").value;
     var what = document.getElementById("what").value;
-    var when1 = document.getElementById("when").value;
+    var when1 = document.getElementById("when1").value;
     var when2 = document.getElementById("when2").value;
     var where = document.getElementById("where").value;
     var why = document.getElementById("why").value;
@@ -17,28 +17,25 @@ function toOutput(intro, who, what, when1, when2, where, why, impact, additional
     document.getElementById('output').value = output;
 }
 
-// function submitData() {
-//     var who = document.getElementById("who").value;
-//     var what = document.getElementById("what").value;
-//     var when1 = document.getElementById("when").value;
-//     var when2 = document.getElementById("when2").value;
-//     var where = document.getElementById("where").value;
-//     var why = document.getElementById("why").value;
-//     var impact = document.getElementById("impact").value;
-//     var additional = document.getElementById("additional").value;
-//     var output = document.getElementById("output").value;
+function submitData() {
+    var intro = document.getElementById("intro").value;
+    var who = document.getElementById("who").value;
+    var what = document.getElementById("what").value;
+    var when1 = document.getElementById("when1").value;
+    var when2 = document.getElementById("when2").value;
+    var where = document.getElementById("where").value;
+    var why = document.getElementById("why").value;
+    var impact = document.getElementById("impact").value;
+    var additional = document.getElementById("additional").value;
+    var output = document.getElementById('output').value;
 
-//     data = {who, what, when1, when2, where, why, impact, additional, output}
-
-//     const options = {
-//         method: "POST",
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//     };
-
-//     const response = await fetch('/api', options);
-//     const json = await response.json();
-//     console.log(json);
-// }
+    var data = {intro, who, what, when1, when2, where, why, impact, additional, output}
+    const options = {
+        method:'POST',
+        body: JSON.stringify(data),
+        header: {
+            'Content-Type': 'application/json'
+        }
+    };
+    fetch('/api', options);
+}
